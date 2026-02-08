ce5fa9aef2db8d13bdbe8e60ab98d1dc5c267e81
IlMali <malighettiluca08@gmail.com>
2026-02-08 15:49:00 +0100

    aggiunta di altro testo
    aggiunta del testo di Islanda
    :

--- ptn.sh
+++ ptn.sh
@@ -2,3 +2,4 @@
 echo "i love PTN"
 echo "la mia canzone preferita è ovviamente Pastello Bianco"
 echo "malighetti per favore non piangere perchè il pastello non è bianco :("
+echo "io Holly e Benjy tu Sailor Moon, storie opposte ma stesso mood"
