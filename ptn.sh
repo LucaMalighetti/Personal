5eb67c5ce3327b18010a4fccbc7bac8b515e14f5
LucaMalighetti <malighetti.luca.08@itisdalmine.edu.it>
2026-02-19 16:28:41 +0100

    Aggiunta PROGETTI PERSONALI

--- ptn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "test"
-echo "i love PTN"
-echo "la mia canzone preferita è ovviamente Pastello Bianco"
-echo "malighetti per favore non piangere perchè il pastello non è bianco :("
-echo "io Holly e Benjy tu Sailor Moon, storie opposte ma stesso mood"
-echo "tu sei bella come casa mia"
